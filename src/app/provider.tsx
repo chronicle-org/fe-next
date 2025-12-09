ecde0e968c59ef8a752956566a9b425fd0a6183e
Adithia Sandi Josua Simangunsong <106661816+AdithiaSandi@users.noreply.github.com>
2025-12-09 08:50:03 +0700

    adjust refetch on focus

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -5,7 +5,16 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  );
 
   return (
     <QueryClientProvider client={queryClient}>
